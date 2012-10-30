0315eb305042384ccb9f6c111c0f3d877304e062
Shaan Sapra <ssapra@uchicago.edu>
2012-10-29 20:34:50 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notecloud::Application.config.secret_token = '74c4498c0fcec1af751c93a77906f351a3ef0ed556a5f81d1f518106e4a8cc3e3edb25d9f7780b1c276128c7023bd7be0d321d720399879abc76a3b12c19ee0b'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Notecloud::Application.routes.draw do
+  get "static_pages/home"
+
+  devise_for :users
+  
+  root :to => "static_pages#home"
+
+end
